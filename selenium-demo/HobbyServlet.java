6b818aac243b14a5d8f2db195f7c9860601ef3d3
Tharuni18 <tharunisai2006@gmail.com>
2026-02-05 10:44:15 +0530

    Hobby Servlet file

---
+++ selenium-demo/HobbyServlet.java
@@ -0,0 +1,28 @@
+package com.example.selenium_demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HobbyServlet")
+public class HobbyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+   
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out= response.getWriter();
+		String[] hobbies = request.getParameterValues("hobby");
+		if(hobbies!=null) {
+			for(String hobby:hobbies) {
+				out.println("<p>"+hobby+"</p>");
+			}
+		}
+
+	}
+}
